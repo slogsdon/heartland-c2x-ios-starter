d006296e1bd8d560fb4a3dfa2e2d7846a7f779c6
Shane Logsdon <shane@shanelogsdon.com>
2022-01-24 14:18:53 -0500

    Initial Commit

---
+++ HeartlandTestAppUITests/HeartlandTestAppUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  HeartlandTestAppUITestsLaunchTests.swift
+//  HeartlandTestAppUITests
+//
+//  Created by Shane Logsdon on 1/24/22.
+//
+
+import XCTest
+
+class HeartlandTestAppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
